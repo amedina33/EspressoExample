a4a1276aae052c613e6ad202c3dec057366d8344
amedina33 <arthur.medina@am.sony.com>
2014-07-31 18:05:03 -0700

    added one working build type that acts different from the release

---
+++ app/src/debug/java/com/ensolabs/espressoexample/debug/PersonDetailsActivity.java
@@ -0,0 +1,27 @@
+package com.ensolabs.espressoexample.debug;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ensolabs.espressoexample.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+import static com.ensolabs.espressoexample.MyActivity.Person;
+
+public class PersonDetailsActivity extends Activity {
+  public static final String EXTRA_PERSON = "person";
+  @InjectView(R.id.fullName) TextView fullNameView;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.person_details_activity);
+    ButterKnife.inject(this);
+
+    Person person = (Person) getIntent().getSerializableExtra(EXTRA_PERSON);
+    fullNameView.setText(String.format("%s, %s", person.getLastName(), person.getFirstName()));
+  }
+}
